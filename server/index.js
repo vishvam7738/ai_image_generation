3f8f265098b2221310ce77098f238523cce9e838
vishvam <vishvamaggarwal@gmail.com>
2024-10-13 15:09:45 +0530

    Setting up MongoDB server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
@@ -8,6 +9,17 @@ const app = express();
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
-app.get('/', aysnc(req, res)=> {
+app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!');
-})
+});
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL)
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
